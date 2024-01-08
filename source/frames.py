cd9a6a59090f8497a4b4a9092a35d11126132a5f
Acer <john.kosinski@gmail.com>
2024-01-08 10:32:22 +0700

    reading frames example

---
+++ source/frames.py
@@ -0,0 +1,14 @@
+import cv2
+vidcap = cv2.VideoCapture('sample.mp4')
+#success,image = vidcap.read()
+count = 0
+success = True
+while success:
+  success,image = vidcap.read()
+  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
+  print(image)
+  
+  count += 1
+  
+  if (count >= 3): 
+      break
