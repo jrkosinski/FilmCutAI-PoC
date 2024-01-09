71ad18c0c3e8db6017f7089b7795051c107ec7ad
Acer <john.kosinski@gmail.com>
2024-01-09 17:42:29 +0700

    playing 'what room?' with more contiguous images

--- source/frames.py
+++ source/frames.py
@@ -6,7 +6,6 @@ success = True
 while success:
   success,image = vidcap.read()
   cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
-  print(image)
   
   count += 1
   
--- source/whatroom.py
+++ source/whatroom.py
@@ -9,79 +9,26 @@ def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
 
-room1_img = encode_image("room-pics/1.jpg")
-room2_img = encode_image("room-pics/2.jpg")
-room3_img = encode_image("room-pics/3.jpg")
-room4_img = encode_image("room-pics/4.jpg")
-room5_img = encode_image("room-pics/5.jpg")
-room6_img = encode_image("room-pics/6.jpg")
-room7_img = encode_image("room-pics/7.jpg")
-room8_img = encode_image("room-pics/8.jpg")
-room9_img = encode_image("room-pics/9.jpg")
-
-payload = [
-    {
-      "role": "user",
-      "content": [
+content = [
         {
           "type": "text",
           "text": "How many different rooms are pictured in this group of images?"
-        },
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": f"data:image/jpeg;base64,{room1_img}"
-          }
-        },
-        #{
-        #  "type": "image_url",
-        #  "image_url": {
-        #    "url": f"data:image/jpeg;base64,{room2_img}"
-        #  }
-        #},
-        #{
-        #  "type": "image_url",
-        #  "image_url": {
-        #    "url": f"data:image/jpeg;base64,{room3_img}"
-        #  }
-        #},
-        #{
-        #  "type": "image_url",
-        #  "image_url": {
-        #    "url": f"data:image/jpeg;base64,{room4_img}"
-        #  }
-        #},
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": f"data:image/jpeg;base64,{room5_img}"
-          }
-        },
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": f"data:image/jpeg;base64,{room6_img}"
-          }
-        },
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": f"data:image/jpeg;base64,{room7_img}"
-          }
-        },
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": f"data:image/jpeg;base64,{room8_img}"
-          }
-        },
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": f"data:image/jpeg;base64,{room9_img}"
-          }
         }
       ]
+  
+for n in range(1, 13): 
+    img = encode_image("room-pics/" + str(n) + ".jpg")
+    content.append({
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{img}"
+          }
+    })
+      
+payload = [
+    {
+      "role": "user",
+      "content": content
     }
   ]
 
@@ -92,6 +39,7 @@ completion = client.chat.completions.create(
     max_tokens=300
 )
 
-print(completion)
+#print(completion)
 print()
 print(completion.choices[0].message)
+print()
