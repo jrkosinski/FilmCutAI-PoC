a8019f2009b5077d35269c565403e5a59d26a8f0
Acer <john.kosinski@gmail.com>
2024-02-01 12:12:24 +0700

    scenesplitter: move files to output directory manually

--- source/classes/scene_splitter.py
+++ source/classes/scene_splitter.py
@@ -1,7 +1,7 @@
 from scenedetect import SceneManager
 from scenedetect.detectors import ContentDetector
 from scenedetect import detect, AdaptiveDetector, split_video_ffmpeg
-import cv2
+import os
 
 class SceneSplitter: 
     def __init__(self): 
@@ -14,7 +14,17 @@ class SceneSplitter:
         if not output_dir.endswith("/"): 
             output_dir += "/"
             
+        #write scenes to files
         if (write_out_scenes): 
-            split_video_ffmpeg(video_path, scene_list) #, output_file_template='$VIDEO_NAME-Scene-$SCENE_NUMBER.')
+            split_video_ffmpeg(video_path, scene_list)
+        
+        #afterwards, move files to the appropriate directory
+        files = os.listdir()
+        for i in range(len(files)): 
+            filename = files[i]
+            #TODO: have to account for different formats
+            if (filename.endswith(".mp4") and filename.find("-Scene-") >= 0): 
+                os.replace("./{0}".format(filename), "{0}{1}".format(output_dir, filename))
+            
         return scene_list
 
