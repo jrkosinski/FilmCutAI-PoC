3f2e4eab269aa9e0217702f7311472e7f51c11b9
Acer <john.kosinski@gmail.com>
2024-02-01 12:18:28 +0700

    clear directory before filling with files

--- source/classes/scene_splitter.py
+++ source/classes/scene_splitter.py
@@ -1,7 +1,7 @@
 from scenedetect import SceneManager
 from scenedetect.detectors import ContentDetector
 from scenedetect import detect, AdaptiveDetector, split_video_ffmpeg
-import os
+import os, shutil
 
 class SceneSplitter: 
     def __init__(self): 
@@ -19,12 +19,28 @@ class SceneSplitter:
             split_video_ffmpeg(video_path, scene_list)
         
         #afterwards, move files to the appropriate directory
-        files = os.listdir()
-        for i in range(len(files)): 
-            filename = files[i]
-            #TODO: have to account for different formats
-            if (filename.endswith(".mp4") and filename.find("-Scene-") >= 0): 
-                os.replace("./{0}".format(filename), "{0}{1}".format(output_dir, filename))
+        
+        if (output_dir != "./"): 
             
-        return scene_list
+            #clear directory first
+            self._clear_output_dir(output_dir)
+            
+            files = os.listdir()
+            for i in range(len(files)): 
+                filename = files[i]
+                #TODO: have to account for different formats
+                if (filename.endswith(".mp4") and filename.find("-Scene-") >= 0): 
+                    os.replace("./{0}".format(filename), "{0}{1}".format(output_dir, filename))
+                
+            return scene_list
 
+    def _clear_output_dir(self, output_dir): 
+        for filename in os.listdir(output_dir):
+            file_path = os.path.join(output_dir, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
